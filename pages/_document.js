c2275b5c798c28585e4a562b68ca6215267205f2
Isaac Garcia <isaacwi@gmail.com>
2021-11-03 22:17:42 -0400

    readme

--- pages/_document.js
+++ pages/_document.js
@@ -60,6 +60,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
+      // eslint-disable-next-line react/display-name
       enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
     });
 
@@ -85,3 +86,5 @@ MyDocument.getInitialProps = async (ctx) => {
     ],
   };
 };
+
+MyDocument.displayName = "MyDoc";
